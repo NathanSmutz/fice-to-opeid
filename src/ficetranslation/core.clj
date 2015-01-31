45b8d87dbdf102e833b78272356d95d9b1c53fb9
unknown <nsmutz@gmail.com>
2015-01-31 11:47:09 -0800

    More Documentation.  Working on a way not to hold onto heads of large CSVs

--- src/ficetranslation/core.clj
+++ src/ficetranslation/core.clj
@@ -10,29 +10,43 @@
   (println "Hello, World!"))
 
 
-(defn add-to-set [the-set element] (if the-set 
-                                     (conj the-set element) 
-                                     #{element}))
+(defn add-to-set
+  "Ads an element to a set and handles the housekeeping of creating a new set if one doesn't exist."
+  [the-set element] 
+  (if the-set 
+    (conj the-set element)
+    #{element}))
 
 
 #_ (def the-file-content (slurp "./resources/ic9596_a.csv"))
 #_ (def the-csv (csv/read-csv the-file-content))
 
-(defn slurp-to-csv [filename]
+(defn slurp-to-csv 
+  "'Slurps' a file and loads it as a LaxySeq of vectors corresponding to each row
+   of the incoming file"
+  [filename]
   (csv/read-csv (slurp filename)))
 
-(defn csv-header-index [csv] (zipmap (first csv) (range)))
+(defn csv-header-index 
+  "Receives a CSV file and returns a map with the items in the header row mapped to their indices in the row"
+  [csv] (zipmap (first csv) (range)))
+
 (defn get-csv-column-by-header 
   "Returns the last column with the given name"
   [csv column-name]
   (let [index  ((csv-header-index csv) column-name)]
     (map #(nth % index) csv)))
 
-(defn extend-fice-opeid-map 
-  [the-map csv-vector fice-header ope-header]
+(defn extend-corelation-map
+  "Extends a map corelating values coresponding to two columns in a CSV table.
+   If values corresponnding to both headers are present in the row,
+   then the second value is added to set refernced by the first value.
+   Receives a map, a LazySeq of Vectors representing a CSV table and 
+   two hashable values represented in the header of the CSV"
+  [the-map csv-vector domain-header range-header]
   (let [csv-index  (csv-header-index csv-vector)
-        fice-index (csv-index fice-header)
-        ope-index  (csv-index ope-header)]    
+        fice-index (csv-index domain-header)
+        ope-index  (csv-index range-header)]    
     (reduce #(update-in %1  [(%2 fice-index)] add-to-set (%2 ope-index))
             the-map
             (filter #(and 
@@ -40,8 +54,14 @@
                        ((complement clojure.string/blank?) (% ope-index))) 
                     (rest csv-vector)))))
 
+;; TODO: Create slurp-csv-into-corelation map
+(defn slurp-csv-into-corelation map
+  [the-map the-file-name first-header second-header]
+  (extend-corelation-map the-map (slurp-to-csv the-file-name) first-header second-header))
+
+
 #_ (def all-fice-opeid 
-  (reduce #(extend-fice-opeid-map %1 %2 "fice" "opeid")
+  (reduce #(extend-corelation-map %1 %2 "fice" "opeid")
           {}
           (map slurp-to-csv ["./resources/ic9596_a.csv" "./resources/ic9697_a.csv" "./resources/ic9798_hdr.csv"])))
 
@@ -49,9 +69,9 @@
 (def all-fice-opeid
   (->
     {}
-    (extend-fice-opeid-map (slurp-to-csv "./resources/ic9596_a.csv")   "fice" "opeid")
-    (extend-fice-opeid-map (slurp-to-csv "./resources/ic9697_a.csv")   "fice" "opeid")
-    (extend-fice-opeid-map (slurp-to-csv "./resources/ic9798_hdr.csv") "fice" "opeid")))
+    (extend-corelation-map (slurp-to-csv "./resources/ic9596_a.csv")   "fice" "opeid")
+    (extend-corelation-map (slurp-to-csv "./resources/ic9697_a.csv")   "fice" "opeid")
+    (extend-corelation-map (slurp-to-csv "./resources/ic9798_hdr.csv") "fice" "opeid")))
 
 ;; This turns out to be a one-to-many situation
 ;; Here are the ones mapping to multiple OPEIDS
@@ -62,3 +82,4 @@
 ;; And intersection on all the sets of multiple OPEIDs returns the empty set
 (apply cset/intersection (map second all-fice-opeid))
 ; #{}
+
